9ce7b3c0179dd32f465f2a6eae5fc09b09b10bc3
Beatrice Akatukunda <beatrice.kunda96@gmail.com>
2020-11-15 17:23:13 +0300

    adds manage-farmer-ones pug view

--- controllers/views.js
+++ controllers/views.js
@@ -1,21 +1,25 @@
 const FarmerOnes = require("../models/farmer-ones");
 
 const login = (req, res) => {
-  console.log(req.url);
-  res.status(200).render("login");
+    console.log(req.url);
+    res.status(200).render("login");
 };
 
 const dashboard = async (req, res) => {
-  const farmerOnes = await FarmerOnes.find({
-    active: true,
-    ward: { $exists: true },
-  });
-  res.status(200).render("ao-dashboard", { farmerOnes });
+    const farmerOnes = await FarmerOnes.find({
+        active: true,
+        ward: { $exists: true },
+    });
+    res.status(200).render("ao-dashboard", { farmerOnes });
 };
 
 const addFarmerOne = (req, res) => {
     res.status(200).render("add-farmer-one");
-  };
-  
+};
 
-module.exports = { login, dashboard, addFarmerOne };
+const managerFarmerOnes = async (req, res) => {
+    const farmerOnes = await FarmerOnes.find({});
+    res.status(200).render("manage-farmer-ones", { farmerOnes });
+};
+
+module.exports = { login, dashboard, addFarmerOne, managerFarmerOnes };
--- routes/views.js
+++ routes/views.js
@@ -6,7 +6,7 @@ router.get('/', viewsController.login);
 router.get('/login', viewsController.login);
 router.get("/ao-dashboard", viewsController.dashboard);
 router.get('/add-farmer-one', viewsController.addFarmerOne);
-// router.get('/manage-farmer-ones', viewsController.managerFarmerOnes);
+router.get('/manage-farmer-ones', viewsController.managerFarmerOnes);
 
 
 module.exports = router;
