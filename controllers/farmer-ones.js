54af5af9ad1df80ebf82f931c31537c29120a710
Beatrice Akatukunda <beatrice.kunda96@gmail.com>
2020-11-15 10:39:18 +0300

    can now edit specific farmer-one's phoneNumber and activities ONLY

--- controllers/farmer-ones.js
+++ controllers/farmer-ones.js
@@ -1,5 +1,20 @@
 const FarmerOnes = require("../models/farmer-ones");
 
+// Helper Function to filter out fields which whould not be updated in the PATCH/PUT route
+// It only takes in the allowed fields
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        // loop through provided obj and check if any of the keys in that obj are eq to the ones you would like to return
+        if (allowedFields.includes(el)) {
+            // if they exist, simply create a new obj with those desired fields
+            newObj[el] = obj[el];
+        }
+    });
+    return newObj;
+};
+
+
 const getAllFarmerOnes = async (req, res) => {
     const farmerOnes = await FarmerOnes.find({});
     res.status(200).json({ message: "success", data: farmerOnes });
@@ -19,13 +34,35 @@ const addNewFarmerOne = async (req, res) => {
 
 const getSpecificFarmerOne = async (req, res) => {
     try {
-      const farmerOne = await FarmerOnes.findById(req.params.id);
-      res.status(200).json({ message: "success", farmerOne: farmerOne });
+        const farmerOne = await FarmerOnes.findById(req.params.id);
+        res.status(200).json({ message: "success", farmerOne: farmerOne });
     } catch (error) {
-      // console.log(error);
-      res.status(404).json({ message: "No FO Found with That Id" });
+        // console.log(error);
+        res.status(404).json({ message: "No FO Found with That Id" });
     }
-  };
+};
 
+const editSpecificFarmerOne = async (req, res) => {
+    // Only allow phoneNumber and activities fields by using the helper filter function
+    const filteredBody = filterObj(req.body, "phoneNumber", "activities");
 
-module.exports = { getAllFarmerOnes, addNewFarmerOne,  getSpecificFarmerOne };
+    try {
+        // Update FO document using the filtered fields
+        const farmerOne = await FarmerOnes.findByIdAndUpdate(
+            req.params.id,
+            filteredBody,
+            { new: true, runValidators: true }
+        );
+        res.status(200).json({
+            message: "successfully updated FO details",
+            farmerOne: farmerOne,
+        });
+    } catch (error) {
+        // console.log(error);
+        res
+            .status(404)
+            .json({ message: "Could not update FO with that ID as it is not Found" });
+    }
+};
+
+module.exports = { getAllFarmerOnes, addNewFarmerOne, getSpecificFarmerOne, editSpecificFarmerOne };
--- routes/farmer-ones.js
+++ routes/farmer-ones.js
@@ -7,6 +7,6 @@ const farmerOneController = require("../controllers/farmer-ones");
 router.get("/", farmerOneController.getAllFarmerOnes);
 router.post("/", farmerOneController.addNewFarmerOne);
 router.get("/:id", farmerOneController.getSpecificFarmerOne);
-// router.patch("/:id", farmerOneController.editSpecificFarmerOne);
+router.patch("/:id", farmerOneController.editSpecificFarmerOne);
 
 module.exports = router;
